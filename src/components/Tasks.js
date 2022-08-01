f4dafe6b9cc9c1ee3420272a3121d8750a47c19d
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-08-01 11:35:55 -0700

    add tasks component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,4 @@
-import { useState } from "react";
-
-const Tasks = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:00pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 3:00pm",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Food Shopping",
-      day: "Feb 7th at 4:00pm",
-      reminder: false,
-    },
-  ]);
+const Tasks = ({ tasks }) => {
 
   return (
     <>
