94473d00bb7ed5011a6838d0d592547b82b51670
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-08-01 11:00:52 -0700

    add header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 };
