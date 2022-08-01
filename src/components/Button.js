0ce8015f83127510228cb22d8edb868a98fadf3f
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-08-01 11:15:06 -0700

    add event handlers

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,24 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-const Button = ({color, text}) => {
-  return <button style={{backgroundColor: color}} className="btn">{text}</button>;
+const Button = ({ color, text }) => {
+  const onClick = (e) => {
+    console.log(e)
+  }
+
+  return (
+    <button onClick={onClick} style={{ backgroundColor: color }} className="btn">
+      {text}
+    </button>
+  );
 };
 
 Button.defaultProps = {
-    color: 'steelblue',
-}
+  color: "steelblue",
+};
 
 Button.propTypes = {
-    color: PropTypes.string,
-    text: PropTypes.string,
-}
+  color: PropTypes.string,
+  text: PropTypes.string,
+};
 
 export default Button;
